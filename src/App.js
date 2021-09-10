854bf8dfdd3b4cf1b1c51bd5820ee805ea522e8f
kundansinghh007 <singh.kundan@protonmail.com>
2021-09-10 20:58:53 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <Home />
     </div>
   );
 }
